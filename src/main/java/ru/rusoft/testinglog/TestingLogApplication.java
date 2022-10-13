7dc0071cf5b7ca4854a068fca3c9e5d96fd2dab7
drovocek <mycodepg@gmail.com>
2022-10-13 18:39:12 +0300

    starter

---
+++ src/main/java/ru/rusoft/testinglog/TestingLogApplication.java
@@ -0,0 +1,13 @@
+package ru.rusoft.testinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rusoft/testinglog/TestingLogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rusoft.testinglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
